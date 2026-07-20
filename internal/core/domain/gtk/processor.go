db115d5d5f94a8c840f1b62a493e50b4c80455d0
Rascal <faiqemilzzz@gmail.com>
2026-07-20 12:57:04 +0700

    fix(gtk-3.0): fix palette that can compatible with old waybar css, move scss logic to assets

--- internal/core/domain/gtk/processor.go
+++ internal/core/domain/gtk/processor.go
@@ -3,55 +3,65 @@ package gtk
 import (
 	"bytes"
 	"os/exec"
+	"path/filepath"
+
 	"theme-engine/internal/core/context"
 	"theme-engine/internal/processor"
 	"theme-engine/internal/renderer"
 )
 
-type GtkProcessor struct {}
+type GtkProcessor struct{}
 
-func (GtkProcessor) Name() string { return "gtk"; }
+func (GtkProcessor) Name() string { return "gtk" }
 
-func init() { processor.RegisterProcessor(GtkProcessor{}); }
+func init() { processor.RegisterProcessor(GtkProcessor{}) }
 
 func (GtkProcessor) Parse(_ any) (any, error) {
-	return nil, nil;
+	return nil, nil
 }
 
 func (GtkProcessor) Resolve(_ any, ctx *context.Context) (any, error) {
 	return Gtk{
 		Config: ctx,
-	}, nil;
+	}, nil
 }
 
-func compileSCSS(templatePath, outputPath string) error {
-	cmd := exec.Command("sassc", templatePath, outputPath);
-
-	var stderr bytes.Buffer;
-	cmd.Stderr = &stderr;
-
-	err := cmd.Run();
-	if err != nil {
-		return err;
+func compileSCSS(src, dst string, loadPaths []string) error {
+	args := append([]string{src, dst}, "-I", src)
+	for _, lp := range loadPaths {
+		args = append(args, "-I", lp)
 	}
+	cmd := exec.Command("sassc", args...)
 
-	return nil;
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil { return err }
+
+	return nil
 }
 
 func (GtkProcessor) Render(
-	templatePath, 
+	templatePath,
 	outputPath string,
 	data any,
 ) error {
-	err := renderer.Render(
-		templatePath, 
-		outputPath + "/scss/source/_source.scss", 
-		data,
-	);
-	if err != nil { return err; }
-	
-	err = compileSCSS(outputPath + "/scss/base/base.scss", outputPath + "/css/source.css");
-	if err != nil { return err; }
+	assetsDir := filepath.Dir(templatePath)
 
-	return compileSCSS(outputPath + "/scss/base/rofi-base.scss", outputPath + "/rasi/source.rasi");
+	err := renderer.Render(
+		templatePath,
+		outputPath+"/scss/source/_source.scss",
+		data,
+	); if err != nil { return err }
+
+	loadPaths := []string{
+		outputPath + "/scss/source",
+		assetsDir,
+	}
+
+	err = compileSCSS(assetsDir+"/base.scss", outputPath+"/css/source.css", loadPaths)
+	if err != nil { return err }
+
+	return compileSCSS(assetsDir+"/rofi-base.scss", outputPath+"/rasi/source.rasi", loadPaths)
 }
